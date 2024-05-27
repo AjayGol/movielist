ae500ef5466ac066486d3ecc3c0a63362c3c386c
Ajay <ajaygoldev@gmail.com>
2024-05-27 09:24:47 +0530

    Fixed eslint

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,22 +1,30 @@
-import React, {useEffect, useRef, useState} from "react";
-import {View, StyleSheet, Image, SafeAreaView, StatusBar, FlatList, Text, ActivityIndicator} from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  View,
+  StyleSheet,
+  Image,
+  SafeAreaView,
+  StatusBar,
+  FlatList,
+  Text,
+  ActivityIndicator,
+} from "react-native";
 import { homeHoldingApi } from "@api";
-import Logo from '../../assets/images/logo.png';
+import Logo from "../../assets/images/logo.png";
 import GenresSelection from "@/components/GenresSelection";
 import MovieCard from "@/components/MovieCard";
 
 export default function TabLayout() {
-
   const flatListRef = useRef();
 
   const [genreData, setGenreData] = useState([]);
-  const [selectedGenre, setSelectedGenre] = useState('all');
+  const [selectedGenre, setSelectedGenre] = useState("all");
   const [moviesData, setMoviesData] = useState([]);
   const [currentYear, setCurrentYear] = useState(2012);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    StatusBar.setBarStyle('light-content', true);
+    StatusBar.setBarStyle("light-content", true);
     homeHoldingApi.getSearchFnoInstruments().then((res) => {
       setGenreData(res?.data?.genres);
     });
@@ -26,15 +34,17 @@ export default function TabLayout() {
 
   useEffect(() => {
     flatListRef.current?.scrollToOffset({ animated: true, offset: 0 });
-  }, [selectedGenre])
+  }, [selectedGenre]);
 
   const fetchMoviesData = async (year) => {
     if (isLoading || year > new Date().getFullYear()) return;
     setIsLoading(true);
     try {
-      const { data: moviesData } = await homeHoldingApi.getListMovie({ primary_release_year: year });
+      const { data: moviesData } = await homeHoldingApi.getListMovie({
+        primary_release_year: year,
+      });
       if (moviesData) {
-        setMoviesData(prevMovies => [...prevMovies, ...moviesData?.results]);
+        setMoviesData((prevMovies) => [...prevMovies, ...moviesData?.results]);
       }
     } finally {
       setIsLoading(false);
@@ -49,53 +59,61 @@ export default function TabLayout() {
 
   const renderFooter = () => {
     return isLoading ? (
-        <ActivityIndicator size="small" color="#fff" style={{marginTop: 20}} />
+      <ActivityIndicator size="small" color="#fff" style={{ marginTop: 20 }} />
     ) : null;
   };
 
   const getData = () => {
-    if (selectedGenre === 'all') { return moviesData };
-    return moviesData?.filter((movie) => movie.genre_ids?.includes(selectedGenre))
-  }
+    if (selectedGenre === "all") {
+      return moviesData;
+    }
+    return moviesData?.filter((movie) =>
+      movie.genre_ids?.includes(selectedGenre),
+    );
+  };
 
   return (
-      <>
-        <SafeAreaView style={styles.mainContainer}>
-          <View style={styles.innerContainer}>
-            <Image source={Logo} style={styles.logo} />
-            <GenresSelection genres={genreData} setSelectedGenre={setSelectedGenre} selectedGenre={selectedGenre} />
-          </View>
-
-          <FlatList
-              ref={flatListRef}
-              data={getData()}
-              renderItem={({ item }) => <MovieCard movie={item} />}
-              keyExtractor={(item) => item.id.toString()}
-              numColumns={2}
-              contentContainerStyle={{ paddingHorizontal: 10 }}
-              style={{ marginTop: 20 }}
-              onEndReached={loadMoreMovies}
-              onEndReachedThreshold={0.5}
-              ListFooterComponent={renderFooter}
+    <>
+      <SafeAreaView style={styles.mainContainer}>
+        <View style={styles.innerContainer}>
+          <Image source={Logo} style={styles.logo} />
+          <GenresSelection
+            genres={genreData}
+            setSelectedGenre={setSelectedGenre}
+            selectedGenre={selectedGenre}
           />
-        </SafeAreaView>
-      </>
+        </View>
+
+        <FlatList
+          ref={flatListRef}
+          data={getData()}
+          renderItem={({ item }) => <MovieCard movie={item} />}
+          keyExtractor={(item) => item.id.toString()}
+          numColumns={2}
+          contentContainerStyle={{ paddingHorizontal: 10 }}
+          style={{ marginTop: 20 }}
+          onEndReached={loadMoreMovies}
+          onEndReachedThreshold={0.5}
+          ListFooterComponent={renderFooter}
+        />
+      </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   safeArea: {
-    backgroundColor: '#121212',
+    backgroundColor: "#121212",
   },
   mainContainer: {
     flex: 1,
-    backgroundColor: '#121212',
+    backgroundColor: "#121212",
   },
   logo: {
-    marginHorizontal: 20
+    marginHorizontal: 20,
   },
   innerContainer: {
-    backgroundColor: '#242424',
-    paddingTop: 10
-  }
+    backgroundColor: "#242424",
+    paddingTop: 10,
+  },
 });
